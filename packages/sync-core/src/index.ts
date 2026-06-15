d0ba8a36f23734046afe10c3e5c84500cd4f5e26
Jaxon <jaxon@floatpoint.net>
2026-06-14 19:07:27 -0500

    feat(sync-core): public barrel exports

---
+++ packages/sync-core/src/index.ts
@@ -0,0 +1,6 @@
+export * from "./types";
+export * from "./clock";
+export * from "./reconciler";
+export * from "./sync-client";
+export * from "./session";
+export { Html5VideoAdapter, type MediaElementLike } from "./adapters/html5-video";
